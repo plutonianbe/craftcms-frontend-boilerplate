1ed5229293100ff3b804f59a9231bc68568d1b3f
Mathias Helin <mathias@plutonian.be>
2020-09-10 16:15:03 +0200

    Add assets folder, webpack config and package.json

---
+++ assets/src/js/index.js
@@ -0,0 +1,8 @@
+import 'default-passive-events'
+
+import '../scss/app.scss'
+import $ from 'jquery'
+window.jQuery = $
+window.$ = $
+import 'popper.js'
+import 'bootstrap/dist/js/bootstrap'
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './assets/src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './web/assets/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env"] }
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
