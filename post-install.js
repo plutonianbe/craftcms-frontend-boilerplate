49a31b38d351e4a649a02989ef6fddda3b5c1b92
Mathias Helin <mathias@plutonian.be>
2020-09-14 14:18:37 +0200

    Try adding npm scripts to the project package.json

--- post-install.js
+++ post-install.js
@@ -17,3 +17,22 @@ var userPath = process.env.INIT_CWD
 
 // Moving files to user's local directory
 gentlyCopy(filesToCopy, userPath)
+
+// Insert scripts
+const saveFile = require('fs').writeFileSync;
+
+const pkgJsonPath = require.main.paths[0].split('node_modules')[0] + 'package.json';
+console.log(require);
+
+const json = require(pkgJsonPath);
+
+
+if (!json.hasOwnProperty('scripts')) {
+  json.scripts = {};
+}
+
+json.scripts['watch'] = 'webpack --watch --mode=development';
+json.scripts['dev'] = 'webpack --mode=development';
+json.scripts['build'] = 'webpack --watch --mode=production';
+
+saveFile(pkgJsonPath, JSON.stringify(json, null, 2));
